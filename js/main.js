c8eb09645e6c308f60d051e3ca8eee4634ac996f
IcedWatermelonJuice <87429695+IcedWatermelonJuice@users.noreply.github.com>
2022-07-09 00:34:14 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,22 +1,14 @@
-$("#ua-detail").ready(function(){
-	var uaData={
-		"完整UA":navigator.userAgent,
-		"版本信息":navigator.appVersion,
-		"系统平台":navigator.platform,
-		"所属公司":navigator.vendor,
-		"内核引擎":navigator.product,
-		"语言环境":navigator.language
+var mediaQuery = window.matchMedia("(prefers-color-scheme: dark)"),
+	themeChange = function() {
+		mediaQuery.matches ? $("html").attr("dark",""):$("html").removeAttr("dark");
 	};
-	var html="";
-	for(let i in uaData){
-		html+="<li><span class='ua-list ua-title'>"+i+":</span><span class='ua-list'>"+(uaData[i]?uaData[i]:"无法识别")+"</span></li>";
-	}
-	$("#ua-detail").html(html);
-	$("#ua-detail").removeClass("hide");
-	$("#ua-wait").addClass("hide");
+themeChange(); // 判断当前模式
+mediaQuery.onchange = themeChange; // 监听模式变化
+
+$("body").ready(()=>{
+	$(".ua-btn").click(() => {
+		parseFn();
+	});
+	parseFn();
 })
-$("#screen-detail").ready(function(){
-	$("#screen-detail").html(screen.width+" × "+screen.height);
-	$("#screen-detail").removeClass("hide");
-	$("#screen-wait").addClass("hide");
-})
+
---
+++ js/parse.js
@@ -0,0 +1,35 @@
+var myParse = function(ua) {
+	ua = typeof ua === "string" && ua.trim() ? ua : navigator.userAgent;
+	var pd1 = new UAParser(ua).getResult(),
+		pd2 = new Browser(ua);
+	return {
+		"ua": ua,
+		"browser": `${pd2.browser?pd2.browser+" ":""}${pd2.version?pd2.version:""}`,
+		"engine": `${pd1.engine.name?pd1.engine.name+" ":""}${pd1.engine.version?pd1.engine.version:""}`,
+		"os": `${pd2.os?pd2.os+" ":""}${pd2.osVersion?pd2.osVersion:""}`,
+		"language": pd2.language ? pd2.language : "",
+		"screen": `${screen.width?screen.width+" × ":""}${screen.height?screen.height:""}`
+	}
+}
+var parseFn = function() {
+	var errorMsg = `<span style="color:red">获取失败</span>`,
+		data = myParse($(".ua-text").val());
+	$(".ua-text").val(data.ua);
+	$(".parse-box-value").html("正在获取");
+	$(".parse-box-value").each((i, e) => {
+		let dataTemp = $(e).attr("data-name");
+		if (dataTemp === "ip") {
+			$.get("https://vv.video.qq.com/checktime?otype=json", function(res) {
+				$(e).text(res['ip'])
+			}, "jsonp");
+			setTimeout(() => {
+				if ($(e).text().trim() === "正在获取") {
+					$(e).html(errorMsg);
+				}
+			}, 3000)
+		} else {
+			dataTemp = data[dataTemp] ? data[dataTemp] : errorMsg;
+			$(e).html(dataTemp)
+		}
+	})
+}
